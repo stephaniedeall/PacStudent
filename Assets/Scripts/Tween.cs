43baf85092e7dc729438179afe5a6e0edaf2c7b5
stephanie deall <stephanie.deall@student.uts.edu.au>
2025-09-22 16:52:42 +1000

    Feature-Movement: Implement complete tweening system for PacStudent movement

--- Assets/Scripts/Tween.cs
+++ Assets/Scripts/Tween.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class Tween : MonoBehaviour
+public class Tween
 {
-    // Start is called before the first frame update
-    void Start()
+    public static IEnumerator Move(Transform transform, Vector3 startPos, Vector3 endPos, float duration, System.Action onComplete = null)
     {
+        float elapsedTime = 0f;
         
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.position = endPos;
+        onComplete?.Invoke();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public static IEnumerator MoveCircular(Transform transform, Vector3[] path, float durationPerSegment, System.Action onComplete = null)
     {
+        int currentIndex = 0;
         
+        while (true) 
+        {
+            Vector3 startPos = path[currentIndex];
+            Vector3 endPos = path[(currentIndex + 1) % path.Length];
+            
+            yield return Move(transform, startPos, endPos, durationPerSegment);
+            
+            currentIndex = (currentIndex + 1) % path.Length;
+        }
     }
 }
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Tweener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Coroutine currentTween;
+    
+    public void StartMove(Transform transform, Vector3 startPos, Vector3 endPos, float duration, System.Action onComplete = null)
     {
+        if (currentTween != null)
+        {
+            StopCoroutine(currentTween);
+        }
         
+        currentTween = StartCoroutine(MoveCoroutine(transform, startPos, endPos, duration, onComplete));
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void StartCircularMovement(Transform transform, Vector3[] path, float durationPerSegment)
     {
+        if (path == null || path.Length < 2)
+        {
+            Debug.LogError("Path must contain at least 2 points!");
+            return;
+        }
         
+        if (currentTween != null)
+        {
+            StopCoroutine(currentTween);
+        }
+        
+        currentTween = StartCoroutine(MoveCircularCoroutine(transform, path, durationPerSegment));
+    }
+    
+    public void StopTween()
+    {
+        if (currentTween != null)
+        {
+            StopCoroutine(currentTween);
+            currentTween = null;
+        }
+    }
+    
+    private IEnumerator MoveCoroutine(Transform transform, Vector3 startPos, Vector3 endPos, float duration, System.Action onComplete)
+    {
+        if (transform == null) yield break;
+        
+        float elapsedTime = 0f;
+        
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.position = endPos;
+        onComplete?.Invoke(); 
+    }
+    
+    private IEnumerator MoveCircularCoroutine(Transform transform, Vector3[] path, float durationPerSegment)
+    {
+        if (transform == null) yield break;
+        
+        int currentIndex = 0;
+        
+        while (true) 
+        {
+            Vector3 startPos = path[currentIndex];
+            Vector3 endPos = path[(currentIndex + 1) % path.Length];
+            
+            yield return MoveCoroutine(transform, startPos, endPos, durationPerSegment, null);
+            
+            currentIndex = (currentIndex + 1) % path.Length;
+        }
     }
 }
