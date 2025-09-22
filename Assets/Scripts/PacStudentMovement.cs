ff03c90fe04394a51f6fabe18f889f471de9d7d8
stephanie deall <stephanie.deall@student.uts.edu.au>
2025-09-22 16:45:41 +1000

    Feature-Movement: Create a PacStudentMovement.cs script to create the movement for the PacStudent

--- Assets/Scripts/PacStudentMovement.cs
+++ Assets/Scripts/PacStudentMovement.cs
@@ -1,18 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class PacStudentMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement Settings")]
+    public float speed = 2f;
+    
+    [Header("Audio Settings")]
+    public AudioClip movementSound;
+    
+    private Vector3[] pathPoints;
+    private Tweener tweener;
+    private AudioSource audioSource;
+    
     void Start()
     {
-        
+        InitializePathPoints();
+        InitializeComponents();
+        StartMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializePathPoints()
     {
+        pathPoints = new Vector3[]
+        {
+            new Vector3(1, 1, 0),      
+            new Vector3(3, 1, 0),      
+            new Vector3(3, 3, 0),     
+            new Vector3(1, 3, 0)      
+        };
         
+        for (int i = 0; i < pathPoints.Length; i++)
+        {
+            pathPoints[i] = transform.TransformPoint(pathPoints[i]);
+        }
     }
-}
+    
+    private void InitializeComponents()
+    {
+        tweener = gameObject.AddComponent<Tweener>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = movementSound;
+        audioSource.loop = true;
+    }
+    
+    private void StartMovement()
+    {
+        float segmentDistance = Vector3.Distance(pathPoints[0], pathPoints[1]);
+        float durationPerSegment = segmentDistance / speed;
+        
+        tweener.StartCircularMovement(transform, pathPoints, durationPerSegment);
+        
+        if (movementSound != null)
+        {
+            audioSource.Play();
+        }
+    }
+        
+    private void OnDrawGizmos()
+    {
+        if (pathPoints == null || pathPoints.Length < 2) return;
+        
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < pathPoints.Length; i++)
+        {
+            int nextIndex = (i + 1) % pathPoints.Length;
+            Gizmos.DrawLine(pathPoints[i], pathPoints[nextIndex]);
+            Gizmos.DrawSphere(pathPoints[i], 0.1f);
+        }
+    }
+    
+    public void StopMovement()
+    {
+        if (tweener != null)
+        {
+            tweener.StopTween();
+        }
+        
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+    
+    public void RestartMovement()
+    {
+        StopMovement();
+        StartMovement();
+    }
+} 
+
